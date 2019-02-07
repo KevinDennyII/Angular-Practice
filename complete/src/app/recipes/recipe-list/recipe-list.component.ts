c1a8757a734ab4fcba49426c99dfda068d674431
Kevin O Denny II <mightyvt@kevins-mbp.home>
2019-02-06 22:39:08 -0600

    progress as of 2/6/2019..

--- complete/src/app/recipes/recipe-list/recipe-list.component.ts
+++ complete/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+  	new Recipe(' A Test Recipe', 'This is simple test', 'http://www.foodreporter.fr/upload/original/5/9/v/s/k/834974.jpg'),
+  	new Recipe(' A Test Recipe', 'This is simple test', 'http://www.foodreporter.fr/upload/original/5/9/v/s/k/834974.jpg')
+  ];
 
   constructor() { }
 
