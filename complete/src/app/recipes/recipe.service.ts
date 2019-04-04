d067661006b755d4f47f7b6183df56fcebdb1232
kevindennyii <kevin.dennyii@gmail.com>
2019-04-04 00:08:16 -0500

    updating since 4/3/2019...only 24% of Complete Angular course finished

--- complete/src/app/recipes/recipe.service.ts
+++ complete/src/app/recipes/recipe.service.ts
@@ -1,6 +1,11 @@
+import {EventEmitter} from '@angular/core';
+
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  //adding event emitter that will hold some Recipe data
+  recipeSelected = new EventEmitter<Recipe>();
+
 	private recipes: Recipe[] = [
   	new Recipe(' A Test Recipe', 'This is simple test', 'http://www.foodreporter.fr/upload/original/5/9/v/s/k/834974.jpg'),
   	new Recipe(' Another Test Recipe', 'This is simple test', 'http://www.foodreporter.fr/upload/original/5/9/v/s/k/834974.jpg')
