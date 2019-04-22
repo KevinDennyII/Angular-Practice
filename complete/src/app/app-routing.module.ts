271b6ddaa799b2adf188a79c4209b931202ad646
kevindennyii <kevin.dennyii@gmail.com>
2019-04-22 17:23:35 -0500

    updated Complete project with App Routing...

---
+++ complete/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import {NgModel} from '@angular/forms';
+import {RecipesComponent} from './recipes/recipes.component';
+import {RouterModule, Routes} from '@angular/router';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {NgModule} from '@angular/core';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'shopping-list', component: ShoppingListComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
