d71d0ab24a34326830ef4a830e217c92bcc3cb67
kevindennyii <kevin.dennyii@gmail.com>
2019-05-27 22:51:13 -0500

    updating practice with Lazy Loading and feature modules...over 65% done with the course

--- complete/src/app/app-routing.module.ts
+++ complete/src/app/app-routing.module.ts
@@ -1,27 +1,12 @@
-import {NgModel} from '@angular/forms';
-import {RecipesComponent} from './recipes/recipes.component';
 import {RouterModule, Routes} from '@angular/router';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {NgModule} from '@angular/core';
-import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
-import {SignupComponent} from './auth/signup/signup.component';
-import {SigninComponent} from './auth/signin/signin.component';
-import {AuthGuard} from './auth/auth-guard.service';
+import {HomeComponent} from './home/home.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  { path: 'recipes', component: RecipesComponent, children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
-      { path: ':id', component: RecipeDetailComponent },
-      { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard] }
-    ]
-  },
-  { path: 'shopping-list', component: ShoppingListComponent },
-  {path: 'signup', component: SignupComponent },
-  {path: 'signin', component: SigninComponent }
+  { path: '', component: HomeComponent},
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'}, // lazy loading the Recipes Module..
+  { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
 @NgModule({
